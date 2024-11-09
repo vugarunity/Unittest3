a94eb4ae6301c6d71161809e9ef01da083201071
vugarunity <159554692+vugarunity@users.noreply.github.com>
2024-11-09 14:54:11 +0400

    Add files via upload

---
+++ coverage/EvenOddNumber.java
@@ -0,0 +1,8 @@
+package seminars.third.coverage;
+
+public class EvenOddNumber {
+    public boolean evenOddNumber(int n) {
+        return n % 2 == 0;
+    }
+}
+
---
+++ coverage/NumberInInterval.java
@@ -0,0 +1,10 @@
+package seminars.third.coverage;
+
+public class NumberInInterval {
+
+    public boolean numberInInterval(int n) {
+        return n > 25 && n < 100;
+    }
+}
+
+
